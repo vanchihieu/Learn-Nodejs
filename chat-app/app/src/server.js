77cc41591ed4d0d00efe18fe7d77a420778b4a4d
Van Chi Hieu <vanchihieu3@gmail.com>
2023-09-06 20:51:10 +0700

    emit && on

--- chat-app/app/src/server.js
+++ chat-app/app/src/server.js
@@ -10,14 +10,30 @@ app.use(express.static(publicPathDirectory));
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// lang nghe su kien ket noi tu client
+let count = 1;
+const message = "Hi everyone";
+
+// lang nghe su kien ket noi tu client (client ket noi voi server)
+// => Emit: gửi sự kiện
+// => On: lắng nghe sự kiện
 io.on("connection", (socket) => {
     console.log("new client connect");
 
+    // nhận lại sự kiện từ client
+    socket.on("send increment client to server", () => {
+        count++;
+        // truyền count từ server về client
+        socket.emit("send count server to client", count);
+    });
+
+    // truyền count từ server về client
+    socket.emit("send count server to client", count);
+    socket.emit("send message server to client", message);
+
     // ngat ket noi
-    socket.on('disconnect', ()=>{
-        console.log('client left server');
-    })
+    socket.on("disconnect", () => {
+        console.log("client left server");
+    });
 });
 
 const port = 3000;
